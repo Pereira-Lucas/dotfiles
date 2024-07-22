440a1f082e7a6ccefafc790af3cc8c1180708c24
Lucas Pereira <lpereira@dickson-constant.com>
2024-07-22 10:46:33 +0200

    Add Terminal settings

--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,4 @@
 New-Item -Path "~\AppData\Local\nvim" -Value ".\nvim" -ItemType SymbolicLink -Force
 
 New-Item -Path "~\.config\starship.toml" -Value ".\.config\starship.toml" -ItemType SymbolicLink -Force
+New-Item -Path "~\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" -Value ".\.config\terminal\settings.json" -ItemType SymbolicLink -Force
