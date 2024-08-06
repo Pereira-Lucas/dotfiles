8c484faa47756315d5214fdbb596cd0409acb302
Lucas Pereira <lpereira@dickson-constant.com>
2024-08-06 13:57:23 +0200

    neovim: ajout marge gauche

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -16,6 +16,20 @@ return {
           DapBreakpoint = { fg = colors.red },
           DapLogPoint = { fg = colors.subtext1 },
           DapStopped = { fg = colors.green },
+
+          -- Couleurs de fond de la marge
+          LineNr = { bg = colors.mantle },
+          CursorLineNr = { bg = colors.mantle },
+          SignColumn = { bg = colors.mantle },
+          GitSignsAdd = { bg = colors.mantle },
+          GitSignsChange = { bg = colors.mantle },
+          GitSignsDelete = { bg = colors.mantle },
+          DiagnosticSignError = { bg = colors.mantle },
+          DiagnosticSignO = { bg = colors.mantle },
+          DiagnosticSignHint = { bg = colors.mantle },
+          DiagnosticSignInfo = { bg = colors.mantle },
+          DiagnosticSignWarn = { bg = colors.mantle },
+          LspInlayHint = { bg = colors.mantle },
         }
       end,
     },
