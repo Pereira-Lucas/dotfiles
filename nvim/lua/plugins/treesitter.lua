8a2fcebb9315bed1dce2f56525aca1752ec4e9d5
Lucas Pereira <lpereira@dickson-constant.com>
2024-10-18 15:33:32 +0200

    ajout treesitter jsdoc

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
   opts = {
-    ensure_installed = { 'bash', 'html', 'lua', 'markdown', 'vim', 'vimdoc', 'vue', 'javascript' },
+    ensure_installed = { 'bash', 'html', 'lua', 'markdown', 'vim', 'vimdoc', 'vue', 'javascript', 'jsdoc' },
 
     auto_install = true,
     highlight = {
