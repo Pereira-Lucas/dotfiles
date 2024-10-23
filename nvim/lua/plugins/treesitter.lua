9681ac811ea3370886d674bde90da21f7cfb1b5d
Lucas <222594@supinfo.com>
2024-10-23 23:13:37 +0200

    add sql syntax in js

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
   opts = {
-    ensure_installed = { 'bash', 'html', 'lua', 'markdown', 'vim', 'vimdoc', 'vue', 'javascript', 'jsdoc' },
+    ensure_installed = { 'bash', 'html', 'lua', 'markdown', 'vim', 'vimdoc', 'vue', 'javascript', 'jsdoc', 'sql' },
 
     auto_install = true,
     highlight = {
---
+++ nvim/queries/javascript/injections.scm
@@ -0,0 +1,23 @@
+ (variable_declarator
+   name: (identifier) @variable_name
+   (#match? @variable_name ".*Sql$")
+   value: [
+		   (string) @injection.content
+		   (template_string) @injection.content
+		   ]
+   (#offset! @injection.content 0 1 0 -1)
+   (#set! injection.include-children)
+   (#set! injection.language "sql")
+)
+
+(assignment_expression
+  left: (identifier) @variable_name
+  (#match? @variable_name ".*Sql")
+  right: [
+		   (string) @injection.content
+		   (template_string) @injection.content
+		   ]
+   (#offset! @injection.content 0 1 0 -1)
+   (#set! injection.include-children)
+   (#set! injection.language "sql")
+  )
