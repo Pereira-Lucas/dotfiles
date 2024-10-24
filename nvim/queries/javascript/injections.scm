0a2398c0585bdef0ef7a20427f890c6ed644c204
Lucas Pereira <lpereira@dickson-constant.com>
2024-10-24 09:49:39 +0200

    better sql integration in js

--- nvim/queries/javascript/injections.scm
+++ nvim/queries/javascript/injections.scm
@@ -1,6 +1,6 @@
  (variable_declarator
    name: (identifier) @variable_name
-   (#match? @variable_name ".*Sql$")
+   (#match? @variable_name ".*Sql$|sql")
    value: [
 		   (string) @injection.content
 		   (template_string) @injection.content
@@ -12,7 +12,7 @@
 
 (assignment_expression
   left: (identifier) @variable_name
-  (#match? @variable_name ".*Sql")
+  (#match? @variable_name ".*Sql$|sql")
   right: [
 		   (string) @injection.content
 		   (template_string) @injection.content
