2b38f53228d9a59f8a081d2fadfdd0228d4f3ece
Lucas Pereira <lpereira@dickson-constant.com>
2025-02-03 10:05:39 +0100

    fix git commands

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -19,10 +19,11 @@ return {
     },
   },
   {
-    'APZelos/blamer.nvim',
+    'TimothyGCY/blamer.nvim',
     cmd = {
       'BlamerToggle',
     },
+    opts = {},
     config = function()
       -- Toggle Git Blame for 5 seconds only
       vim.api.nvim_create_user_command('BlamerToggleShort', function()
@@ -33,34 +34,34 @@ return {
     keys = {
       { '<leader>gb', '<cmd>BlamerToggleShort<cr>', desc = '[G]it [B]lame' },
     },
-    {
-      'isakbm/gitgraph.nvim',
-      opts = {
-        symbols = {
-          merge_commit = 'M',
-          commit = '*',
-        },
-        format = {
-          timestamp = '%H:%M:%S %d-%m-%Y',
-          fields = { 'hash', 'timestamp', 'author', 'branch_name', 'tag' },
-        },
-        hooks = {
-          on_select_commit = function(commit)
-            print('selected commit:', commit.hash)
-          end,
-          on_select_range_commit = function(from, to)
-            print('selected range:', from.hash, to.hash)
-          end,
-        },
+  },
+  {
+    'isakbm/gitgraph.nvim',
+    opts = {
+      symbols = {
+        merge_commit = 'M',
+        commit = '*',
       },
-      keys = {
-        {
-          '<leader>gg',
-          function()
-            require('gitgraph').draw({}, { all = true, max_count = 5000 })
-          end,
-          desc = '[G]it[G]raph - Draw',
-        },
+      format = {
+        timestamp = '%H:%M:%S %d-%m-%Y',
+        fields = { 'hash', 'timestamp', 'author', 'branch_name', 'tag' },
+      },
+      hooks = {
+        on_select_commit = function(commit)
+          print('selected commit:', commit.hash)
+        end,
+        on_select_range_commit = function(from, to)
+          print('selected range:', from.hash, to.hash)
+        end,
+      },
+    },
+    keys = {
+      {
+        '<leader>gg',
+        function()
+          require('gitgraph').draw({}, { all = true, max_count = 5000 })
+        end,
+        desc = '[G]it[G]raph - Draw',
       },
     },
   },
