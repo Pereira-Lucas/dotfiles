da54496b35c3d5cd0ca57a70851a5c2f10adf922
Lucas Pereira <lpereira@dickson-constant.com>
2025-02-17 17:56:20 +0100

    git log

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -10,59 +10,28 @@ return {
     'kdheepak/lazygit.nvim',
     cmd = {
       'LazyGit',
+      'LazyGitLog',
     },
     dependencies = {
       'nvim-lua/plenary.nvim',
     },
     keys = {
       { '<leader>go', '<cmd>LazyGit<cr>', desc = 'Lazy[G]it [O]pen' },
+      { '<leader>gg', '<cmd>LazyGitLog<cr>', desc = 'Lazy[G]it [G]raph' },
     },
   },
   {
     'TimothyGCY/blamer.nvim',
-    cmd = {
-      'BlamerToggle',
-    },
     opts = {},
     config = function()
       -- Toggle Git Blame for 5 seconds only
       vim.api.nvim_create_user_command('BlamerToggleShort', function()
-        vim.cmd.BlamerToggle()
-        vim.defer_fn(vim.cmd.BlamerToggle, 5000)
+        vim.cmd.BlamerShow()
+        vim.defer_fn(vim.cmd.BlamerHide, 5000)
       end, {})
     end,
     keys = {
       { '<leader>gb', '<cmd>BlamerToggleShort<cr>', desc = '[G]it [B]lame' },
-    },
-  },
-  {
-    'isakbm/gitgraph.nvim',
-    opts = {
-      symbols = {
-        merge_commit = 'M',
-        commit = '*',
-      },
-      format = {
-        timestamp = '%H:%M:%S %d-%m-%Y',
-        fields = { 'hash', 'timestamp', 'author', 'branch_name', 'tag' },
-      },
-      hooks = {
-        on_select_commit = function(commit)
-          print('selected commit:', commit.hash)
-        end,
-        on_select_range_commit = function(from, to)
-          print('selected range:', from.hash, to.hash)
-        end,
-      },
-    },
-    keys = {
-      {
-        '<leader>gg',
-        function()
-          require('gitgraph').draw({}, { all = true, max_count = 5000 })
-        end,
-        desc = '[G]it[G]raph - Draw',
-      },
     },
   },
 }
