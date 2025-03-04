eca0fda64dcf21ac5e307d444e516f83188776ae
Lucas Pereira <lpereira@dickson-constant.com>
2025-03-04 09:56:04 +0100

    tree-sitter context

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,25 +1,33 @@
 return {
-  -- Highlight, edit, and navigate code
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
-  opts = {
-    ensure_installed = { 'bash', 'html', 'lua', 'markdown', 'vim', 'vimdoc', 'vue', 'javascript', 'jsdoc', 'sql' },
+  {
+    -- Highlight, edit, and navigate code
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    opts = {
+      ensure_installed = { 'bash', 'html', 'lua', 'markdown', 'vim', 'vimdoc', 'vue', 'javascript', 'jsdoc', 'sql' },
 
-    auto_install = true,
-    highlight = {
-      enable = true,
-      disable = function(_, buf)
-        local max_filesize = 100 * 1024 -- 100 KB
-        local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
-        if ok and stats and stats.size > max_filesize then
-          return true
-        end
-      end,
+      auto_install = true,
+      highlight = {
+        enable = true,
+        disable = function(_, buf)
+          local max_filesize = 100 * 1024 -- 100 KB
+          local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
+          if ok and stats and stats.size > max_filesize then
+            return true
+          end
+        end,
+      },
+      indent = { enable = true },
     },
-    indent = { enable = true },
+    config = function(_, opts)
+      require('nvim-treesitter.install').compilers = { 'clang' }
+      require('nvim-treesitter.configs').setup(opts)
+    end,
   },
-  config = function(_, opts)
-    require('nvim-treesitter.install').compilers = { 'clang' }
-    require('nvim-treesitter.configs').setup(opts)
-  end,
+  {
+    'nvim-treesitter/nvim-treesitter-context',
+    opts = {
+      max_lines = 3,
+    },
+  },
 }
