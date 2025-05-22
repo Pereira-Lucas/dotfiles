575141d68565dae52d2afa9589d5bf289d40fe88
Lucas Pereira <lpereira@dickson-constant.com>
2025-05-22 11:10:54 +0200

    git

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,10 +1,13 @@
 return {
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
+    lazy = false,
     opts = {},
-    -- keys = {
-    --   { '<leader>gd', '<cmd>GitSigns diffthis<cr>', desc = '[G]it [D]iff' },
-    -- },
+    keys = {
+      { '<leader>gb', '<cmd>Gitsigns blame_line<cr>', desc = '[G]it [B]lame' },
+      { '<leader>gl', '<cmd>Gitsigns setqflist<cr>', desc = '[G]it [L]ist changes' },
+      { '<leader>gp', '<cmd>Gitsigns preview_hunk_inline<cr>', desc = '[G]it [P]review hunk' },
+    },
   },
   {
     'kdheepak/lazygit.nvim',
@@ -18,20 +21,6 @@ return {
     keys = {
       { '<leader>go', '<cmd>LazyGit<cr>', desc = 'Lazy[G]it [O]pen' },
       { '<leader>gg', '<cmd>LazyGitLog<cr>', desc = 'Lazy[G]it [G]raph' },
-    },
-  },
-  {
-    'TimothyGCY/blamer.nvim',
-    opts = {},
-    config = function()
-      -- Toggle Git Blame for 5 seconds only
-      vim.api.nvim_create_user_command('BlamerToggleShort', function()
-        vim.cmd.BlamerShow()
-        vim.defer_fn(vim.cmd.BlamerHide, 5000)
-      end, {})
-    end,
-    keys = {
-      { '<leader>gb', '<cmd>BlamerToggleShort<cr>', desc = '[G]it [B]lame' },
     },
   },
 }
